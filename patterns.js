d3d46eff4d3b0a7ede36130ba916d73d11e9c3d1
Tunniedrey <115155502+Tunniedrey@users.noreply.github.com>
2023-01-27 13:49:00 +0100

    Add initial patterns files

---
+++ patterns.js
@@ -0,0 +1,23 @@
+class phoneNumber {
+    constructor (AddPhoneNumber,RemoviePhoneNumber,DialPhoneNumber) {
+        this.observers = new set ()
+    }
+    add(AddPhoneNumber) {
+        this.AddPhoneNumber.add(090112233);
+        this.AddPhoneNumber.add(080223344);
+        this.AddPhoneNumber.add(070334455);
+    }
+    Remove(RemoviePhoneNumber) {
+        this.RemoviePhoneNumber.delete()
+    }
+    dial(DialPhoneNumber) {
+        this.DialPhoneNumber.call()
+    }
+    notify(DialPhoneNumber) {
+        for(let observers of this.DialPhoneNumber);
+    }
+
+    const = new Notification ('incoming call');
+
+    
+}
