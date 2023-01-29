d89ab51884ac19f9baaaf65cbf895aaadd582d11
Tunniedrey <115155502+Tunniedrey@users.noreply.github.com>
2023-01-29 16:56:53 +0100

    Update patterns.js

--- patterns.js
+++ patterns.js
@@ -1,23 +1,50 @@
-class phoneNumber {
-    constructor (AddPhoneNumber,RemoviePhoneNumber,DialPhoneNumber) {
-        this.observers = new set ()
+class telephone {
+    constructor(){
+        this.phoneNumbers = [];
     }
-    add(AddPhoneNumber) {
-        this.AddPhoneNumber.add(090112233);
-        this.AddPhoneNumber.add(080223344);
-        this.AddPhoneNumber.add(070334455);
+    addPhoneNumber(observer) {
+        this.phoneNumbers.push(observer);
     }
-    Remove(RemoviePhoneNumber) {
-        this.RemoviePhoneNumber.delete()
+    removePhoneNumber(observer) {
+        const index =
+        this.phoneNumbers.indexOf(observer);
+        if (index > -1){
+            this.phoneNumbers.splice(index,1);
+        }
     }
-    dial(DialPhoneNumber) {
-        this.DialPhoneNumber.call()
+    dialPhoneNumber(observer) {
+        const index =
+        this.phoneNumbers.indexOf(observer);
+        if (index > 1) {
+            const phoneNumber =
+        this.phoneNumbers[index];
+            phoneNumber.print();
+            phoneNumber.notify();
+        } else {
+            console.log("Phone number doesn't exist");
+        }
     }
-    notify(DialPhoneNumber) {
-        for(let observers of this.DialPhoneNumber);
-    }
-
-    const = new Notification ('incoming call');
-
-    
 }
+
+class observer {
+    constructor(observer) {
+        this.observer = observer;
+    }
+    print() {
+        console.log(this.observer);
+    }
+    notify() {
+        console.log("Now Dialling" + this.observer);
+    }
+}
+
+const caller = new telephone();
+const phoneNumber1 = new
+observer ("112233");
+
+caller.addPhoneNumber(phoneNumber1);
+caller.addPhoneNumber(phoneNumber2);
+caller.removePhoneNumber(phoneNumber2);
+
+caller.dialPhoneNumber(phoneNumber1);
+caller.dialPhoneNumber(phoneNumber2);
